de2b35c4250754a6280789f8e11bad2e6fe4fd1a
Oier Bravo Urtasun <oierbravo@gmail.com>
2018-02-08 00:38:34 +0100

    First commit

---
+++ Assets/Scripts/BikolabsFirebase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Unity.Editor;
+
+public class BikolabsFirebase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+public class MyScript : MonoBehaviour
+{
+  void Start()
+  {
+    // Set this before calling into the realtime database.
+    FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://bikolabs-ar-test.firebaseio.com/");
+  }
+}
+
+// Update is called once per frame
+void Update () {
+		
+	}
+}
